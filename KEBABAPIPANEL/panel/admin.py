17888c9d2f95e1fc5d709a8f7b8a1bdf1b3185bd
Oskar Dąbrowski <oskar.ariel.dabrowski@gmail.com>
2025-01-02 11:15:16 +0100

    Added social links field to Kebab model and API

--- KEBABAPIPANEL/panel/admin.py
+++ KEBABAPIPANEL/panel/admin.py
@@ -5,3 +5,6 @@ from api.models import Kebab
 class KebabAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'opening_hours']
     search_fields = ['name', 'status']
+    fields = ['name', 'description', 'latitude', 'longitude', 'opening_hours',
+              'contact', 'meats', 'sauces', 'status', 'craft_rating',
+              'order_methods', 'location_details', 'social_links']
