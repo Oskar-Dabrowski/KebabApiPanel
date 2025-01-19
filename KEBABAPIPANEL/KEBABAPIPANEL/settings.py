bb8fa50e756a7f76ee278b210f16794d3e6a7282
ACendlewski <adamc01@interia.eu>
2025-01-19 22:25:20 +0100

    seeders

--- KEBABAPIPANEL/KEBABAPIPANEL/settings.py
+++ KEBABAPIPANEL/KEBABAPIPANEL/settings.py
@@ -137,6 +137,10 @@ if not DEBUG:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
