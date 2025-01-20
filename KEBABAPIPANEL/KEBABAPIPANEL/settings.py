70b9b6ff6805acca6ff85db41f92ec143a79f713
ACendlewski <adamc01@interia.eu>
2025-01-20 11:03:37 +0100

    Update settings.py

--- KEBABAPIPANEL/KEBABAPIPANEL/settings.py
+++ KEBABAPIPANEL/KEBABAPIPANEL/settings.py
@@ -134,9 +134,8 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
